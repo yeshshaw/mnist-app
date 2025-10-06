b5d766746e88efe1648004d3d9cc70191858aa23
yesh shaw <nihassid@gmail.com>
2025-10-06 11:17:35 +0530

    Update script.js for backend URL fix

--- static/script.js
+++ static/script.js
@@ -157,17 +157,19 @@ predictBtn.addEventListener('click', () => {
     }
 
     // ✅ Backend URL fixed to port 8000
-    fetch('http://127.0.0.1:8000/predict', {
-        method: 'POST',
-        headers: { 'Content-Type':'application/json' },
-        body: JSON.stringify({pixels: pixels})
-    })
-    .then(res => res.json())
-    .then(data => {
-        result.innerText = data.error ? data.error : "Prediction: " + data.prediction;
-    })
-    .catch(err => {
-        result.innerText = "Error connecting to backend!";
-        console.error(err);
-    });
+   const BASE_URL = window.location.origin;
+
+fetch(`${BASE_URL}/predict`, {
+    method: 'POST',
+    headers: { 'Content-Type':'application/json' },
+    body: JSON.stringify({ pixels: pixels })
+})
+.then(res => res.json())
+.then(data => {
+    result.innerText = data.error ? data.error : "Prediction: " + data.prediction;
+})
+.catch(err => {
+    result.innerText = "Error connecting to backend!";
+    console.error(err);
+});
 });
